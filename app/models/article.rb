9d6e9becb5e3aac3691f4c55750dc654488bc868
oj-ochko <73417322+oj-ochko@users.noreply.github.com>
2022-05-22 22:12:29 +0800

    nav-bar

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
-    include Visible
-  
-    has_many :comments, dependent: :destroy
-  
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
-  end
-  
+  belongs_to :user
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
